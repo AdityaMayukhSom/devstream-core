6b0868dae67f4dd4b11c64e343dabef6ddb44c5c
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-09-17 12:22:40 +0530

    mapper objects to convert dto to domain objects

---
+++ src/main/java/in/devstream/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package in.devstream.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import in.devstream.dto.UserDto;
+import in.devstream.model.User;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    public UserDto userToUserDto(User user);
+
+    @Mapping(target = "enabled", ignore = true)
+    @Mapping(target = "roles", ignore = true)
+    public User userDtoToUser(UserDto userDto);
+}
