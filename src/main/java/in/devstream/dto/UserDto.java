c1a6fbcc430a9b34a56f15e833937a452c3ab369
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-09-17 12:22:17 +0530

    dto oject to receive data from client

---
+++ src/main/java/in/devstream/dto/UserDto.java
@@ -0,0 +1,12 @@
+package in.devstream.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private String id;
+    private String email;
+    private String password;
+    private String fullname;
+}
