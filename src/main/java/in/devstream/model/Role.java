28294199c44e18073b837f89f193fba795f85278
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-11-24 01:42:02 +0530

    Refactored role objects to be singleton

--- src/main/java/in/devstream/model/Role.java
+++ src/main/java/in/devstream/model/Role.java
@@ -1,21 +1,41 @@
 package in.devstream.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 @Entity
-@Table(name = "roles", indexes = {
-    @Index(columnList = "role", unique = true)})
+@Table(name = "roles",
+        indexes = {
+            @Index(columnList = "role", unique = true)})
 public class Role {
 
-    public static final String ADMIN = "ADMIN";
-    public static final String USER = "USER";
+    public static final Role ADMIN = new Role("ADMIN");
+    public static final Role USER = new Role("USER");
+    public static final Role VIEWER = new Role();
+
+    // https://docs.jboss.org/hibernate/orm/3.5/reference/en/html/persistent-classes.html
+    // All persistent classes must have a default constructor (which can be non-public) 
+    // so that Hibernate can instantiate them using Constructor.newInstance().
+    private Role() {
+        this("VIEWER");
+    }
+
+    private Role(String role) {
+        this.role = role;
+    }
 
     @Id
-    private String role;
-
+    @NotBlank
+    @NotEmpty
+    @NonNull
+    @Column(unique = true, nullable = false, updatable = false)
+    private final String role;
 }
--- src/main/java/in/devstream/service/AuthService.java
+++ src/main/java/in/devstream/service/AuthService.java
@@ -11,7 +11,6 @@ import in.devstream.dto.UserDto;
 import in.devstream.mapper.UserMapper;
 import in.devstream.model.Role;
 import in.devstream.model.User;
-import in.devstream.repository.RoleRepository;
 import in.devstream.repository.UserRepository;
 
 @Service
@@ -23,17 +22,11 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private RoleRepository roleRepository;
-
     public void registerUser(UserDto userDto) {
-        User user = UserMapper.INSTANCE.userDtoToUser(userDto);
+        User user = UserMapper.MAPPER.toUser(userDto);
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        Role role = roleRepository.findByRole(Role.ADMIN);
         Set<Role> roles = new HashSet<>();
-        if (role != null) {
-            roles.add(role);
-        }
+        roles.add(Role.ADMIN);
         user.setRoles(roles);
         userRepository.save(user);
     }
