2db7d47ed55553a7651482dae6b613cbbf54fbe8
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-11-24 01:46:03 +0530

    Added table constraints and creation, updation date in models

--- src/main/java/in/devstream/model/User.java
+++ src/main/java/in/devstream/model/User.java
@@ -2,38 +2,74 @@ package in.devstream.model;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.ToString;
 
 @Data
 @Entity
-@Table(name = "users", indexes = {
-    @Index(unique = true, columnList = "email")})
+@ToString
+@Table(name = "users",
+        indexes = {
+            @Index(unique = true, columnList = "email")},
+        uniqueConstraints = {
+            @UniqueConstraint(name = "UniqueEmail", columnNames = {"email"})})
 public class User implements UserDetails {
 
     @Id
+    @NotBlank
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
+    @NonNull
+    @NotBlank
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @NonNull
+    @NotBlank
     private String password;
 
+    @NonNull
+    @NotBlank
     private String fullname;
 
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+
     private boolean enabled;
+
+    private boolean accountExpired;
+
+    private boolean accountLocked;
+
+    private boolean credentialsExpired;
 
     // https://stackoverflow.com/questions/42334475/spring-jpa-users-roles-authentication-how-do-i-avoid-duplicate-role-entries
     @OneToMany(mappedBy = "role", cascade = CascadeType.DETACH, orphanRemoval = false)
@@ -58,16 +94,16 @@ public class User implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return !this.accountExpired;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return !this.accountLocked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return !this.credentialsExpired;
     }
 }
